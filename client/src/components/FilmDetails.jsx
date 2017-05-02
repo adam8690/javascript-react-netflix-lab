024e59f79ba047a909df4fe6d3267dd7d0f8c18d
kimnorth <benignhumour@gmail.com>
2017-05-02 15:52:26 +0100

    Final submission before return to class.

--- client/src/components/FilmDetails.jsx
+++ client/src/components/FilmDetails.jsx
@@ -1,1 +1,48 @@
 import React from 'react'
+
+
+class FilmDetails extends React.Component{
+  
+  constructor(props){
+    super(props)
+    this.state = {
+      image: this.props.film.poster
+    }
+  }
+
+componentWillMount(){
+  console.log(this.state)
+  const imgUrl = this.props.film.poster;
+  
+  const request = new XMLHttpRequest();
+    request.open('GET', imgUrl);
+    request.onload = () => {
+      if(request.status === 200){
+        return
+      }
+      this.setState({image: "https://slack-imgs.com/?c=1&url=https%3A%2F%2Fpbs.twimg.com%2Fprofile_images%2F600060188872155136%2Fst4Sp6Aw.jpg"})
+    }
+    request.send()
+  }
+
+render(){
+  console.log('rendering...')
+  console.log(this.props)
+  console.log(this.state)
+  return (
+      <div className="film-details" >
+        <h3>{this.props.film.show_title}</h3>
+        <img src={this.props.film.poster}/>
+        <p>Release Year: {this.props.film.release_year}</p>
+        <p>Summary: {this.props.film.summary}</p>
+      </div>
+    )
+  }
+
+
+
+
+
+}
+
+export default FilmDetails
--- client/src/containers/DirectorContainer.jsx
+++ client/src/containers/DirectorContainer.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import FilmSelector from '../components/FilmSelector.jsx'
+import FilmDetails from '../components/FilmDetails.jsx'
 
 class DirectorContainer extends React.Component {
 
@@ -7,17 +8,17 @@ class DirectorContainer extends React.Component {
     super(props)
     this.state = {
       filmArray: [],
-      selectedFilm: null
+      selectedFilm: {}
     }
   }
 
   componentDidMount(){
-    const url = "http://netflixroulette.net/api/api.php?director=Steven%20SpielBerg"
+    const url = "http://netflixroulette.net/api/api.php?director=Woody%20Allen"
     const request = new XMLHttpRequest();
     request.open('GET', url);
     request.onload = () => {
       if(request.status === 200){
-        console.log(request.responseText)
+        console.log(request)
         const jsonString = request.responseText;
         const filmData = JSON.parse(jsonString);
         this.setState({filmArray: filmData, selectedFilm: filmData[0]})
@@ -36,8 +37,9 @@ class DirectorContainer extends React.Component {
     return (
 
       <div className="director-container">
-        <h1>Steven Spielberg</h1>
+        <h1>Woody Allen</h1>
         <FilmSelector films={this.state.filmArray} handleStateChange={this.handleStateChange.bind(this)}/> 
+        <FilmDetails film={this.state.selectedFilm}/>
       </div>
 
     )
