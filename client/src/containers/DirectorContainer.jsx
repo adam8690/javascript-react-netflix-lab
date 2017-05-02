eaf5111109433436534f6fab4a8e1ee37742789e
kimnorth <benignhumour@gmail.com>
2017-05-02 13:35:29 +0100

    Created containers and components. Updated app to show director container.

---
+++ client/src/containers/DirectorContainer.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+class DirectorContainer extends React.Component {
+
+  constructor(props){
+    super(props)
+  }
+
+  render(){
+
+    return (
+
+      <div className="director-container">
+        <h1>Steven Spielberg</h1>
+      </div>
+
+    )
+
+
+  }
+
+}
+
+export default DirectorContainer;
